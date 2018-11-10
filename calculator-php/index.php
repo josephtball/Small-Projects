0e2d72df5c31ceafa0b8e4db386a60d897a9390c
Joseph T. Ball <jball321@live.com>
2018-11-09 22:48:00 -0500

    started a php version of the calculator.

---
+++ calculator-php/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Calculator</title>
+    <meta name="author" content="Joseph T. Ball">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
+    <link rel="shortcut icon" href="icon.ico" type="image/vnd.microsoft.icon">
+    <!-- Latest compiled and minified CSS for Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <header>
+    </header>
+
+    <nav>
+    </nav>
+
+    <main class="container">
+        <div class="row">
+            <div id="calculator" class="col">
+                <div class="row">
+                    <div id="display" class="col-9"></div>
+                    <div id="clear" class="col-3 function-key"><-</div>
+                </div>
+                <div class="row">
+                    <section id="keypad" class="col">
+                        <div class="row">
+                            <div id="7" class="col-3 number-key">7</div>
+                            <div id="8" class="col-3 number-key">8</div>
+                            <div id="9" class="col-3 number-key">9</div>
+                            <div id="/" class="col-3 function-key">/</div>
+                        </div>
+                        <div class="row">
+                            <div id="4" class="col-3 number-key">4</div>
+                            <div id="5" class="col-3 number-key">5</div>
+                            <div id="6" class="col-3 number-key">6</div>
+                            <div id="*" class="col-3 function-key">x</div>
+                        </div>
+                        <div  class="row">
+                            <div id="1" class="col-3 number-key">1</div>
+                            <div id="2" class="col-3 number-key">2</div>
+                            <div id="3" class="col-3 number-key">3</div>
+                            <div id="-" class="col-3 function-key">-</div>
+                        </div>
+                        <div class="row">
+                            <div id="0" class="col-3 offset-3 number-key">0</div>
+                            <div id="decimal" class="col-3 number-key">.</div>
+                            <div  id="+" class="col-3 function-key">+</div>
+                        </div>
+                        <div class="row">
+                            <div id="solve" class="col-12 function-key">=</div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>      
+    </main>
+
+    <footer>
+    </footer>
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript for Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
